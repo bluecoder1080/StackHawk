6c7e81fb07b0f54a97f2290228dc84b79fbdaf41
adityasingh1080z@gmail.com <adityasingh1080z@gmail.com>
2025-05-26 14:12:08 +0530

    Validate admin signup data with Zod

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,26 @@
 const { Router } = require("express");
 const adminRouter = Router();
 const { adminModel } = require("../db");
+const { z } = require("zod");
 
+////////////////////////////////////////////////////////////////////
+// SignupZod Schema
+const signupSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(5),
+  firstName: z.string().min(6),
+  lastName: z.string().min(6),
+});
+////////////////////////////////////////////////////////////////////
 adminRouter.post("/signup", function (req, res) {
-  res.json({
-    message: "This is admin Signup endpoint",
-  });
+  const validation = signupSchema.safeParse(req.body);
+  if (!validation.success) {
+    return res.status(400).json({
+      error: validation.error.errors.map((err) => err.message),
+    });
+  }
+
+  const { email, password, firstName, lastName } = validation.data;
 });
 adminRouter.post("/signin", function (req, res) {
   res.json({
@@ -14,9 +29,7 @@ adminRouter.post("/signin", function (req, res) {
 });
 
 ///api/v1/admin/
-adminRouter.post("/course", function (req, res) {
-  
-});
+adminRouter.post("/course", function (req, res) {});
 adminRouter.put("/course", function (req, res) {});
 
 adminRouter.get("/course/bulk", function (req, res) {});
