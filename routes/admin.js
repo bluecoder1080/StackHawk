db25a3ad6f4e1b883260a0da3c75c87e314038bb
adityasingh1080z@gmail.com <adityasingh1080z@gmail.com>
2025-05-26 13:45:07 +0530

    Added Some Scripts

--- routes/admin.js
+++ routes/admin.js
@@ -3,14 +3,22 @@ const adminRouter = Router();
 const { adminModel } = require("../db");
 
 adminRouter.post("/signup", function (req, res) {
-  
   res.json({
     message: "This is admin Signup endpoint",
   });
 });
-adminRouter.post("/signin", function (req, res) {});
-adminRouter.post("/course", function (req, res) {});
+adminRouter.post("/signin", function (req, res) {
+  res.json({
+    message: "This is admin SignIN endpoint",
+  });
+});
+
+///api/v1/admin/
+adminRouter.post("/course", function (req, res) {
+  
+});
 adminRouter.put("/course", function (req, res) {});
+
 adminRouter.get("/course/bulk", function (req, res) {});
 
 module.exports = { adminRouter };
